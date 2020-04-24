2e6ebacc1d90677429972402c8a466db02ba2f66
ChamkhiAnas <mahmoudbennouna@gmail.com>
2020-04-24 00:24:44 +0000

    Laravel7 With Jwt

---
+++ app/Task.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    //
+        /**
+     * @var string
+     */
+    protected $table = 'tasks';
+
+    /**
+     * @var array
+     */
+    protected $guarded = [];
+
+}
